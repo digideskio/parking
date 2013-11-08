336fa00de2f14aaa21f19de2c97ff485c4b49029
Bobbilee Hartman <bobbileehartman@gmail.com>
2013-11-07 20:35:49 -0600

    another debugging with the result and address request

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,13 @@ class HomeController < ApplicationController
     if Rails.env.test? || Rails.env.development?
       @current_user_location = '405 Howard Street San Francisco, CA, 94105'
     else
-      result = request.geocode
+      result = request.location
       @current_user_location = result.address
-      Rails.logger.info(current_user_location) 
+      Rails.logger.info(@current_user_location) 
     end
     # if you want to list out the ones near you as well 
       # @bike_parking_spots_within_one_mile = BikeParkingSpot.near(@current_user_location, 0.3)
       # @bike_parking_spots = @bike_parking_spots_within_one_mile.paginate(:page => params[:page], :per_page => 9)
-    binding.pry
     @bike_parking_spots_nearby = BikeParkingSpot.near(@current_user_location, 0.4)
     @hash = Gmaps4rails.build_markers(@bike_parking_spots_nearby) do |bike_parking_spot, marker|
       marker.lat bike_parking_spot.latitude
