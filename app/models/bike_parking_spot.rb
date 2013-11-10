c80ccf358ffccf24b880b88341440be12ff59352
Bobbilee Hartman <bobbileehartman@gmail.com>
2013-11-10 12:25:29 -0600

    adding logic back into table on index

--- app/models/bike_parking_spot.rb
+++ app/models/bike_parking_spot.rb
@@ -2,6 +2,7 @@ class BikeParkingSpot < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, :if => :address_changed? 
 
+  # for importing new data
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       self.create! row.to_hash
